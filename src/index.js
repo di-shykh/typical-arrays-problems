6e43c7918e61d3ebffce14ee450baad49fd3fa41
di-shykh <di49@mail.ru>
2021-08-17 02:50:44 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 exports.min = function min (array) {
   array.sort((a,b)=>a-b);
   return array[0];
-}
+};
 
 exports.max = function max (array) {
   array.sort((a,b)=>b-a);
   return array[0];
-}
+};
 
 exports.avg = function avg (array) {
   const average = array.reduce((a, b) => a + b) / array.length;
   return average;
-}
+};
